0efbc2a9da038c6dc7a3b8da76f1c37b089c367e
Anh. Phạm Tuấn (16) <anhpt16@vng.com.vn>
2026-04-21 15:55:16 +0700

    fix

--- src/models/encoders/text_generation/__init__.py
+++ src/models/encoders/text_generation/__init__.py
@@ -1,6 +1,6 @@
 # Text generation sub-package for LLM-based description generation.
 from .llm_wrapper import LLMWrapper
-from .base_generator import BaseTextGenerator
+from .base_generator import BaseTextGenerator, GenerationConfig
 from .hf_local_generator import HFLocalGenerator
 from .prompt_builder import PromptBuilder
 from .output_cleaner import OutputCleaner
@@ -9,6 +9,7 @@ from .cache_manager import CacheManager
 __all__ = [
     "LLMWrapper",
     "BaseTextGenerator",
+    "GenerationConfig",
     "HFLocalGenerator",
     "PromptBuilder",
     "OutputCleaner",
